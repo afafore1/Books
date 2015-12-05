8dd05f8363861769e9b182619e8b5c383d16776f
Ayomitunde Fafore <Ayomitunde Fafore>
2015-12-05 10:40:00 -0500

    updated register please revise

--- login/register.php
+++ login/register.php
@@ -1,20 +1,48 @@
 <?php
-	include("../config/dbconfig.php");
-	$connection = new mysqli($host, $username, $password, $dbname);
+	require("../config/dbconfig.php");
+	$connection = new mysqli($host, $username, $password, $dbname); // what is this for ?
+	if(!empty($_POST)){
+
 	$email = $_POST["email"];
 	$password = $_POST["password"];
 	$firstName = $_POST["firstName"];
 	$lastName = $_POST["lastName"];
-
-	$sql = "SELECT * FROM `users` WHERE LOWER(email) = LOWER(\"$email\")";
-	$records = $connection->query($sql);
-	if ($records->num_rows > 0) {
-		header("Location: ../general/alert.php?alert=This email was already used&direct=../login/login.html");
-	} else {
-		$sql = "INSERT INTO `Users` (email, firstname, lastname, password)"
-				." VALUES (\"$email\", \"$firstName\", \"$lastName\", \""
-				.md5($password)."\")";
-		$connection->query($sql);
-		header("Location: ../general/alert.php?alert=Registration successful!&direct=../login/register.html");		
+	
 	}
+	$query = "SELECT 1 FROM Users WHERE email = :email";
+	$query_params = array(':email' => $email);
+	try{
+		$stmt = $db->prepare($query);
+		$result = $stmt->execute($query_params);
+	}catch (PDOException $ex){
+		$response["success"] = 0;
+		$response["message"] = "Database error. Please try again"; // this lines might need to be changed
+		die(json_encode($response));
+	}
+
+	$row = $stmt->fetch();
+	if($row){
+		$response["success"] = 0;
+		header("Location: ../general/alert.php?alert=This email was already used&direct=../login/login.html");
+		// die ?
+	}else{
+		$query = "INSERT INTO Users (firstname,lastname,email,password) VALUES ( :first, :last, :email, :pass)";
+		$query_params = array(
+			':first' => $firstName,
+			':last' => $lastName,
+			':email' => $email,
+			':password' => md5($password)
+			);
+	}
+
+	try{
+		$stmt = $db->prepare($query);
+		$result = $stmt->execute($query_params);
+	}catch (PDOException $ex){
+      $response["success"] = 0; // it failed :(
+      $response["message"] = "Database error. Please try again";
+      die(json_encode($response));
+    }
+    $response["success"] = 1; // if we are here then registration successful
+    header("Location: ../general/alert.php?alert=Registration successful!&direct=../login/register.html");
 ?>
